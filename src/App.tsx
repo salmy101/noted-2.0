f0ce73ff3cbdf2fe4799082788b5365a1fd06b7f
Salma Ibrahim <salma.ibrahim1307@gmail.com>
2023-10-10 10:28:08 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<h1>Home</h1>} />
+      <Route path="/new" element={<h1>New</h1>} />
+      <Route path="*" element={<Navigate to="/" />} />
+
+
+    </Routes>
+  );
+}
+
+export default App;
